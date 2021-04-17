5bf81dd7fac9d4515dbb5a30135a0d574aac1154
Jorge <jorgelartearteagap@gmail.com>
2021-04-17 13:11:23 -0400

    servidor

--- data/db.js
+++ data/db.js
@@ -2,7 +2,8 @@ import e from 'express';
 import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/remote', {useNewUrlParser : true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost/remote', {useNewUrlParser : true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://tembiapo:Tembiapo123@cluster0.h1lb5.mongodb.net/remote?retryWrites=true&w=majority', {useNewUrlParser : true, useUnifiedTopology: true});
 
 const jobsSchema = new mongoose.Schema({
     company: String,
