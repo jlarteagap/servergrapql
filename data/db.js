6be19fbca9fe934e2b2cfe1286c1f1395d1a1035
Jorge <jorgelartearteagap@gmail.com>
2021-04-17 15:29:40 -0400

    fix: refactor hard code conecction string to Heroku config

--- data/db.js
+++ data/db.js
@@ -1,9 +1,11 @@
 import e from 'express';
 import mongoose from 'mongoose';
 
+
+const uri = process.env.MONGODB_URI;
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost/remote', {useNewUrlParser : true, useUnifiedTopology: true});
-mongoose.connect('mongodb+srv://tembiapo:Tembiapo123@cluster0.h1lb5.mongodb.net/remote?retryWrites=true&w=majority', {useNewUrlParser : true, useUnifiedTopology: true});
+mongoose.connect(uri, {useNewUrlParser : true, useUnifiedTopology: true});
 
 const jobsSchema = new mongoose.Schema({
     company: String,
