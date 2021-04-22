f239d4de7934138fcb2217ce7c14a00c1ea4cc6a
Jorge <jorgelartearteagap@gmail.com>
2021-04-21 22:13:18 -0400

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express'
-
+import cors from 'cors'
 import { ApolloServer } from 'apollo-server-express';
 
 import { typeDefs } from './data/schema.js';
 import { resolvers } from './data/resolvers.js'
 
 const app = express();
+app.use(cors())
+
 const server = new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true,});
 server.applyMiddleware({app});
 
