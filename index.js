cd6a02d283c4a7e66d6d3a8dd0d771c48f45352f
Jorge <jorgelartearteagap@gmail.com>
2021-06-25 21:44:08 -0400

    caching error

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ import { resolvers } from './data/resolvers.js'
 
 dotenv.config({path: 'variables.env'})
 const app = express();
-app.use(cors())
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true
+}
 
 const server = new ApolloServer({ 
 	typeDefs, 
-	resolvers, 
+	resolvers,
 	introspection: true, 
 	playground: true,
 	context: async ({req}) => {
@@ -22,15 +26,15 @@ const server = new ApolloServer({
 
 		if(token !== null){
 			try{
-				const userActual = jwt.verify(token, process.env.SECRET)
+				const userActual = await jwt.verify(token, process.env.SECRET)
 				return userActual
-			} catch {
-				console.log(error.message)
+			} catch(err) {
+				console.error(err)
 			}
 		}
 	}
 });
-server.applyMiddleware({app});
+server.applyMiddleware({app, cors: corsOptions});
 
 app.listen(process.env.PORT || 4000, 
 	() => console.log("Server is runningo en ${server.graphqlPath}`"))
