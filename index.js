d597fa4d8fa11b6a05400791fc45bf0e03f98eba
Jorge <jorgelartearteagap@gmail.com>
2021-07-19 19:41:20 -0400

    adding cors

--- index.js
+++ index.js
@@ -6,23 +6,45 @@ import { resolvers } from './data/resolvers.js';
 import { typeDefs } from './data/typeDefs.js';
 import path from 'path';
 
+import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 const port = process.env.PORT || 4000
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true
+}
 
 async function startServer() {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    introspection: true,
+    playground: true,
+    context: async({req}) => {
+      const token = req.headers.authorization || '';
+
+      if(token !== null){
+        try{
+          const userActual = await jwt.verify(token, process.env.SECRET)
+          return userActual
+        } catch(err){
+          console.error(err)
+        }
+      }
+    }
   });
   await server.start();
   const app = express();
   // This middleware should be added before calling `applyMiddleware`.
   app.use(graphqlUploadExpress());
+
   app.use(express.static(path.resolve('./public')));
 
-  server.applyMiddleware({ app });
+  server.applyMiddleware({ app, cors: corsOptions });
 
   await new Promise(r => app.listen({ port }, r));
 
