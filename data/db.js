5724eff79ec0f1fc74f4d0a2ab8943a59dc5da6f
Jorge <jorgelartearteagap@gmail.com>
2021-11-05 11:38:14 -0400

    process env server

--- data/db.js
+++ data/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const uri = process.env.MONGODB_URI || "mongodb://localhost/remote"
+const uri = process.env.MONGODB_URI
 
 mongoose.Promise = global.Promise
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
